810fe4d628636c70ae6e51f5bc5a20ca06032846
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-26 13:21:20 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Ihor Chernyshev
+ * Created on: Oct 2023
  * This program ...
 */
 
-basic.showString('Hello, World!')
+let disctance = 0
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+  basic.clearScreen()
+  disctance = sonar.ping(
+    DigitalPin.P1, 
+    DigitalPin.P2, 
+    PingUnit.Centimeters
+  )
+  basic.showNumber(disctance)
+  basic.showIcon(IconNames.Happy)
+})
