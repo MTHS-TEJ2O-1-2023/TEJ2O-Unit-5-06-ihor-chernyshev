ed10280d044fbccb3ed98761ea5a8e5a4b1195ab
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-29 15:56:35 +0000

    puthon version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,24 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Ihor Chernyshev
+Created on: Oct 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+class HCSR04:
+    # this class abstracts out the functionality of the HC-SR04 and
+    # returns distance in mm
+    # Trig: pin 1
+    # Echo: pin 2
+    ...
+
+
+sonar = HCSR04()
+display.clear
+display.show(Image.HAPPY)
+
+while True:
+    display.show(sonar.distance_mm() / 10)
+    sleep(500)
