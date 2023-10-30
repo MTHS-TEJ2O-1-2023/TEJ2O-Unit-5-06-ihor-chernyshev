69337100a54a51fe43996771119ab4af0024c3f4
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-30 22:16:47 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -4,21 +4,26 @@ Created on: Oct 2023
 This module is a Micro:bit MicroPython program
 """
 
+# setup
 from microbit import *
+from machine import time_pulse_us
 
-
-class HCSR04:
-    # this class abstracts out the functionality of the HC-SR04 and
-    # returns distance in mm
-    # Trig: pin 1
-    # Echo: pin 2
-    ...
-
-
-sonar = HCSR04()
-display.clear
+display.clear()
 display.show(Image.HAPPY)
 
+tring = pin1
+echo = pin2
+
+tring.write_digital(1)
+echo.read_digital()
+
+# finding the distance
 while True:
-    display.show(sonar.distance_mm() / 10)
-    sleep(500)
+    if button_a.was_pressed():
+        tring.write_digital(1)
+        tring.write_digital(0)
+        micros = time_pulse_us(echo, 2)
+        t_echo = micros / 1000000
+        dist_cm = (t_echo / 2) * 34300
+        display.scroll(str(int(dist_cm)))
+        display.show(Image.HAPPY)
